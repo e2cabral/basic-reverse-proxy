d0234c4c1ec79e7c69fbb7c4003a180488d46299
Edson Cabral Junior <e2cabraljunior@gmail.com>
2024-02-18 07:18:54 -0300

    chore: replace proxy initialization with ParseToProxy function
    The `main` function in `main.go` now uses the `ParseToProxy` function to retrieve and parse proxy services. This replaces the previous initialization method, enhancing flexibility by handling services retrieved from the header. The addition also necessitated the import of the "reverse-proxy-test/data" and "slices" packages.

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"net/http"
-	"reverse-proxy-test/proxy"
+	"reverse-proxy-test/data"
+	"slices"
 )
 
 func main() {
-	parser, err := proxy.NewURIParser("https://jsonplaceholder.typicode.com")
+	var proxyServices data.ProxyServices
+	proxyServices.Load()
+	services, err := proxyServices.GetServices()
 	if err != nil {
 		panic(err)
 	}
 
-	http.HandleFunc("/", parser.Handle)
+	proxies := proxyServices.ParseToProxy(services)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		index := slices.IndexFunc(services, func(s data.URLService) bool {
+			return r.Header.Get("X-Route-To") == s.Key
+		})
+		if index == -1 {
+			http.Error(w, "Invalid service key", http.StatusBadRequest)
+			return
+		}
+		proxies[index].Handle(w, r)
+	})
 	err = http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic(err)
